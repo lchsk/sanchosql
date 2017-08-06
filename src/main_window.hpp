0c145e4b15f89f25dbec44f3a23b9f7b0b2bcf97
Maciej Lechowski <mjlechowski@gmail.com>
2017-08-06 16:07:28 +0100

    Add MainWindow

---
+++ src/main_window.hpp
@@ -0,0 +1,55 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include <gtkmm/application.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/treeview.h>
+#include <gtkmm/textview.h>
+#include <gtkmm/notebook.h>
+#include <gtkmm/liststore.h>
+#include <gtkmm/treestore.h>
+#include <gtkmm/buttonbox.h>
+#include <gtkmm/scrolledwindow.h>
+#include <gtkmm/stock.h>
+
+
+class MainWindow : public Gtk::Window
+{
+public:
+    MainWindow();
+    virtual ~MainWindow() {};
+
+protected:
+  class BrowserModel : public Gtk::TreeModel::ColumnRecord
+  {
+  public:
+
+      BrowserModel()
+      {
+          add(table);
+          add(table_catalog);
+          add(table_schema);
+      }
+
+      Gtk::TreeModelColumn<Glib::ustring> table;
+      Gtk::TreeModelColumn<Glib::ustring> table_catalog;
+      Gtk::TreeModelColumn<Glib::ustring> table_schema;
+  };
+
+private:
+    BrowserModel browser_model;
+
+    void on_tab_close_button_clicked(Glib::ustring);
+    void on_browser_row_activated(const Gtk::TreeModel::Path& path,
+                                  Gtk::TreeViewColumn* column);
+
+    Gtk::Box box;
+    Gtk::TreeView browser;
+    Glib::RefPtr<Gtk::TreeStore> browser_store;
+    Gtk::ScrolledWindow browser_scrolled_window;
+    Gtk::Notebook notebook;
+};
+
+#endif
