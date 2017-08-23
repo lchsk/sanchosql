b3ed468f7d36850994a5e584c86e9759ad654386
Maciej Lechowski <mjlechowski@gmail.com>
2017-08-24 00:19:15 +0100

    Pass PostgresConnection to main window

--- src/pc.cpp
+++ src/pc.cpp
@@ -8,12 +8,17 @@
 
 int main (int argc, char *argv[])
 {
-    PostgresConnection pc("127.0.0.1", "", "", "", 5434);
+    std::shared_ptr<PostgresConnection> pc
+        = std::make_shared<PostgresConnection>("127.0.0.1",
+                                               "postgres",
+                                               "",
+                                               "",
+                                               5434);
     auto app = Gtk::Application::create(argc, argv, "postgres.client");
 
-    MainWindow main_window;
+    MainWindow main_window(pc);
 
-    main_window.insert_tables(pc.get_db_tables());
+    main_window.insert_tables(pc->get_db_tables());
 
     return app->run(main_window);
 }
