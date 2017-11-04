3d658c1a6ea0d81562f2726f492f4d9fb79ca3d2
Maciej Lechowski <mjlechowski@gmail.com>
2017-11-04 23:03:20 +0000

    Store all connections; new functions for adding and retrieving them

--- src/connections.cpp
+++ src/connections.cpp
@@ -11,5 +11,26 @@ namespace san
         conn->password = "sancho";
         conn->dbname = "sancho";
         conn->port = "5432";
+
+        add("sancho", "localhost", "sancho", "sancho", "sancho", "5432");
+    }
+
+    void Connections::add(const Glib::ustring& name,
+                          const std::string& host,
+                          const std::string& user,
+                          const std::string& password,
+                          const std::string& dbname,
+                          const std::string& port)
+    {
+        auto conn = std::make_shared<san::ConnectionDetails>();
+
+        conn->name = name;
+        conn->host = host;
+        conn->user = user;
+        conn->password = password;
+        conn->dbname = dbname;
+        conn->port = port;
+
+        connections[name] = conn;
     }
 }
--- src/connections.hpp
+++ src/connections.hpp
@@ -2,8 +2,12 @@
 #define CONNECTIONS_HPP
 
 #include <memory>
+#include <map>
+
+#include <glibmm.h>
 
 #include "connection_details.hpp"
+#include "util.hpp"
 
 namespace san
 {
@@ -16,10 +20,29 @@ namespace san
             return conn;
         }
 
+        void add(const Glib::ustring& name,
+                 const std::string& host,
+                 const std::string& user,
+                 const std::string& password,
+                 const std::string& dbname,
+                 const std::string& port);
+
         static Connections* instance() { return &ins; }
+
+        const std::map<Glib::ustring, std::shared_ptr<san::ConnectionDetails>>&
+        get_connections() const {
+            return connections;
+        };
+
+        bool exists(const Glib::ustring& conn_name) const {
+            return IN_MAP(connections, conn_name);
+        };
 
     private:
         std::shared_ptr<san::ConnectionDetails> conn;
+        std::map<Glib::ustring,
+                 std::shared_ptr<san::ConnectionDetails>> connections;
+
         static Connections ins;
     };
 }
