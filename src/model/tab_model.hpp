52fd5951f1a4d3d3cda53ec4ea3fd6d1eae0a369
Maciej Lechowski <mjlechowski@gmail.com>
2017-11-19 15:48:28 +0000

    Add function that accepts changes to the table

--- src/model/tab_model.hpp
+++ src/model/tab_model.hpp
@@ -1,6 +1,8 @@
 #ifndef TAB_MODEL_HPP
 #define TAB_MODEL_HPP
 
+#include <set>
+
 #include <gtkmm.h>
 
 #include "../pg_conn.hpp"
@@ -80,7 +82,70 @@ namespace san
 			return primary_key.size();
 		}
 
+		const std::vector<san::PrimaryKey> get_primary_key() const {
+			return primary_key;
+		}
+
+		void accept_changes() {
+			std::stringstream query;
+
+			query << "update "
+				  << table_name
+				  << " ";
+
+			std::stringstream where;
+			std::stringstream values;
+
+			for	(const auto& el : pk) {
+				where << " where ";
+				bool first_where = true;
+
+				for (const auto& key : el.first) {
+					if (! first_where) {
+						where << " and ";
+					}
+
+					where << key.first
+						  << " = '"
+						  << key.second
+						  << "'";
+
+					first_where = false;
+				}
+
+				values << " set ";
+				bool first_value = true;
+
+				for (const auto& value : el.second) {
+					if (! first_value) {
+						values << ", ";
+					}
+
+					values << value.first
+						   << " = '"
+						   << value.second
+						   << "'";
+
+					first_value = false;
+				}
+			}
+
+			query << values.str() << where.str() << "; commit;";
+
+			g_debug("Accept query: %s", query.str().c_str());
+
+			conn().run_query(query.str());
+
+			pk.clear();
+		}
+
 		const std::string get_query() const;
+
+		std::vector<std::map<std::string, std::string>> original_pk;
+		std::vector<std::map<std::string, std::string>> to_update;
+		std::vector<Glib::ustring> columns_to_update;
+
+		std::map<std::set<std::pair<Glib::ustring, Glib::ustring>>, std::unordered_map<std::string, Glib::ustring>> pk;
 
 	private:
 		const std::string get_order_by_query() const;
