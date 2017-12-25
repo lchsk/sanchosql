c1d673ea144e27c938a8fb60b3d9c69573a534cf
Maciej Lechowski <mjlechowski@gmail.com>
2017-12-25 14:03:06 +0000

    Change test test_util -> test_string

---
+++ tests/test_string.cpp
@@ -0,0 +1,49 @@
+#include <map>
+
+#include "gtest/gtest.h"
+
+#include "../src/string.hpp"
+
+TEST(Util, contains_only_numbers)
+{
+    EXPECT_TRUE(san::string::contains_only_numbers("123"));
+    EXPECT_TRUE(san::string::contains_only_numbers(""));
+    EXPECT_FALSE(san::string::contains_only_numbers("abc0"));
+    EXPECT_FALSE(san::string::contains_only_numbers("1abc"));
+}
+
+TEST(Util, replace_all)
+{
+    EXPECT_EQ(san::string::replace_all("abc def", "def", "abc"), "abc abc");
+}
+
+TEST(Util, trim)
+{
+    EXPECT_EQ(san::string::trim("abc"), "abc");
+    EXPECT_EQ(san::string::trim(" abc "), "abc");
+    EXPECT_EQ(san::string::trim(" a b c "), "a b c");
+    EXPECT_EQ(san::string::trim("  "), "");
+    EXPECT_EQ(san::string::trim(""), "");
+}
+
+TEST(Util, is_empty)
+{
+    EXPECT_TRUE(san::string::is_empty(""));
+    EXPECT_TRUE(san::string::is_empty("  "));
+    EXPECT_FALSE(san::string::is_empty("abc"));
+    EXPECT_FALSE(san::string::is_empty("    abc "));
+}
+
+TEST(Util, in_map_macro)
+{
+    std::map<std::string, int> m { {"abc", 1} };
+
+    EXPECT_TRUE(IN_MAP(m, "abc"));
+    EXPECT_FALSE(IN_MAP(m, "test"));
+}
+
+int main (int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
