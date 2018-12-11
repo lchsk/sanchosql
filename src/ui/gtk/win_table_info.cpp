623cdc27b6d389b2546e4afef50236eff8834f20
lchsk <mjlechowski@gmail.com>
2018-12-11 23:08:23 +0000

    Show table stats in gui

--- src/ui/gtk/win_table_info.cpp
+++ src/ui/gtk/win_table_info.cpp
@@ -3,20 +3,19 @@
 namespace sancho {
 namespace ui {
 namespace gtk {
-TableInfoWindow::TableInfoWindow(
-    BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder> &builder)
+TableInfoWindow::TableInfoWindow(BaseObjectType *cobject,
+                                 const Glib::RefPtr<Gtk::Builder> &builder)
     : Gtk::Window(cobject), builder(builder) {
 
     builder->get_widget("notebook_tabs", notebook_tabs);
     builder->get_widget("box_columns", box_columns);
     builder->get_widget("box_constraints", box_constraints);
     builder->get_widget("box_indexes", box_indexes);
+    builder->get_widget("box_stats", box_stats);
     builder->get_widget("btn_close", btn_close);
 
-    signal_show().connect(
-        sigc::mem_fun(*this, &TableInfoWindow::on_win_show));
-    signal_hide().connect(
-        sigc::mem_fun(*this, &TableInfoWindow::on_win_hide));
+    signal_show().connect(sigc::mem_fun(*this, &TableInfoWindow::on_win_show));
+    signal_hide().connect(sigc::mem_fun(*this, &TableInfoWindow::on_win_hide));
     btn_close->signal_clicked().connect(
         sigc::mem_fun(*this, &TableInfoWindow::on_btn_close_clicked));
 
@@ -28,7 +27,8 @@ TableInfoWindow::TableInfoWindow(
 
     schema_model = Gtk::TreeStore::create(schema_columns);
     tree_schema.set_model(schema_model);
-    tree_schema.set_grid_lines(Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
+    tree_schema.set_grid_lines(
+        Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
 
     tree_schema.append_column("Position", schema_columns.col_pos);
     tree_schema.append_column("Column name", schema_columns.col_name);
@@ -38,13 +38,15 @@ TableInfoWindow::TableInfoWindow(
 
     // Set up constraints tab
     scrolled_constraints.add(tree_constraints);
-    scrolled_constraints.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    scrolled_constraints.set_policy(Gtk::POLICY_AUTOMATIC,
+                                    Gtk::POLICY_AUTOMATIC);
 
     box_constraints->pack_start(scrolled_constraints);
 
     constraints_model = Gtk::TreeStore::create(constraints_columns);
     tree_constraints.set_model(constraints_model);
-    tree_constraints.set_grid_lines(Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
+    tree_constraints.set_grid_lines(
+        Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
 
     tree_constraints.append_column("Name", constraints_columns.col_name);
     tree_constraints.append_column("Constraint", constraints_columns.col_check);
@@ -57,26 +59,44 @@ TableInfoWindow::TableInfoWindow(
 
     indexes_model = Gtk::TreeStore::create(indexes_columns);
     tree_indexes.set_model(indexes_model);
-    tree_indexes.set_grid_lines(Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
+    tree_indexes.set_grid_lines(
+        Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
 
     tree_indexes.append_column("Name", indexes_columns.col_name);
     tree_indexes.append_column("Definition", indexes_columns.col_definition);
 
+    // Set up stats tab
+    scrolled_stats.add(tree_stats);
+    scrolled_stats.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+
+    box_stats->pack_start(scrolled_stats);
+
+    stats_model = Gtk::TreeStore::create(stats_columns);
+    tree_stats.set_model(stats_model);
+    tree_stats.set_grid_lines(
+        Gtk::TreeViewGridLines::TREE_VIEW_GRID_LINES_BOTH);
+
+    tree_stats.append_column("Name", stats_columns.col_name);
+    tree_stats.append_column("Value", stats_columns.col_value);
+    tree_stats.append_column("Description", stats_columns.col_description);
+
     show_all_children();
 }
 
-void TableInfoWindow::init(sancho::db::PostgresConnection& conn,
-                           const std::string& schema_name, const std::string& table_name)
-{
-  set_title("Table Information for " + schema_name + "." + table_name + " - SanchoSQL");
+void TableInfoWindow::init(sancho::db::PostgresConnection &conn,
+                           const std::string &schema_name,
+                           const std::string &table_name) {
+    set_title("Table Information for " + schema_name + "." + table_name +
+              " - SanchoSQL");
 
-  load_columns_data(conn, schema_name, table_name);
-  load_constraints_data(conn, schema_name, table_name);
-  load_indexes_data(conn, schema_name, table_name);
+    load_columns_data(conn, schema_name, table_name);
+    load_constraints_data(conn, schema_name, table_name);
+    load_indexes_data(conn, schema_name, table_name);
+    load_stats_data(conn, schema_name, table_name);
 }
-  void TableInfoWindow::load_columns_data(sancho::db::PostgresConnection& conn,
-                           const std::string& schema_name, const std::string& table_name)
-  {
+void TableInfoWindow::load_columns_data(sancho::db::PostgresConnection &conn,
+                                        const std::string &schema_name,
+                                        const std::string &table_name) {
     schema_model->clear();
 
     std::shared_ptr<sancho::QueryResult> result =
@@ -85,78 +105,80 @@ void TableInfoWindow::init(sancho::db::PostgresConnection& conn,
                        conn.get_columns_query(schema_name, table_name));
 
     if (!result->success) {
-      return;
+        return;
     }
 
-    const auto& data = result->as_map();
+    const auto &data = result->as_map();
 
-    for (const auto& row_data : data) {
-      Gtk::TreeModel::Row row = *(schema_model->append());
+    for (const auto &row_data : data) {
+        Gtk::TreeModel::Row row = *(schema_model->append());
 
-      std::string data_type = "";
-      std::string char_len = "";
+        std::string data_type = "";
+        std::string char_len = "";
 
-      for (const auto& pair : row_data) {
-        const std::string& key = pair.first;
-        const std::string& value = pair.second;
+        for (const auto &pair : row_data) {
+            const std::string &key = pair.first;
+            const std::string &value = pair.second;
 
-        if (key == "ordinal_position") {
-          row[schema_columns.col_pos] = value;
-        } else if (key == "column_name") {
-          row[schema_columns.col_name] = value;
-        } else if (key == "column_default") {
-          row[schema_columns.col_default] = value;
-        } else if (key == "is_nullable") {
-          row[schema_columns.col_is_nullable] = value == "YES" ? "yes" : "no";
-        } else if (key == "data_type") {
-          data_type = value;
-        } else if (key == "character_maximum_length") {
-          char_len = value;
+            if (key == "ordinal_position") {
+                row[schema_columns.col_pos] = value;
+            } else if (key == "column_name") {
+                row[schema_columns.col_name] = value;
+            } else if (key == "column_default") {
+                row[schema_columns.col_default] = value;
+            } else if (key == "is_nullable") {
+                row[schema_columns.col_is_nullable] =
+                    value == "YES" ? "yes" : "no";
+            } else if (key == "data_type") {
+                data_type = value;
+            } else if (key == "character_maximum_length") {
+                char_len = value;
+            }
         }
-      }
 
-      if (!char_len.empty()) {
-        data_type = data_type + " (" + char_len + ")";
-      }
+        if (!char_len.empty()) {
+            data_type = data_type + " (" + char_len + ")";
+        }
 
-      row[schema_columns.col_type] = data_type;
+        row[schema_columns.col_type] = data_type;
     }
 }
 
-  void TableInfoWindow::load_constraints_data(sancho::db::PostgresConnection& conn,
-                                              const std::string& schema_name, const std::string& table_name)
-  {
+void TableInfoWindow::load_constraints_data(
+    sancho::db::PostgresConnection &conn, const std::string &schema_name,
+    const std::string &table_name) {
     constraints_model->clear();
 
-    std::shared_ptr<sancho::QueryResult> result =
-        conn.run_query(sancho::QueryType::NonTransaction,
-                       conn.get_check_constraints_query(schema_name, table_name),
-                       conn.get_columns_query(schema_name, table_name));
+    std::shared_ptr<sancho::QueryResult> result = conn.run_query(
+        sancho::QueryType::NonTransaction,
+        conn.get_check_constraints_query(schema_name, table_name),
+        conn.get_columns_query(schema_name, table_name));
 
     if (!result->success) {
-      return;
+        return;
     }
 
-    const auto& data = result->as_map();
+    const auto &data = result->as_map();
 
-    for (const auto& row_data : data) {
-      Gtk::TreeModel::Row row = *(constraints_model->append());
+    for (const auto &row_data : data) {
+        Gtk::TreeModel::Row row = *(constraints_model->append());
 
-      for (const auto& pair : row_data) {
-        const std::string& key = pair.first;
-        const std::string& value = pair.second;
+        for (const auto &pair : row_data) {
+            const std::string &key = pair.first;
+            const std::string &value = pair.second;
 
-        if (key == "check_name") {
-          row[constraints_columns.col_name] = value;
-        } else if (key == "check_constraint") {
-          row[constraints_columns.col_check] = value;
+            if (key == "check_name") {
+                row[constraints_columns.col_name] = value;
+            } else if (key == "check_constraint") {
+                row[constraints_columns.col_check] = value;
+            }
         }
-      }
     }
-  }
-  void TableInfoWindow::load_indexes_data(sancho::db::PostgresConnection& conn,
-                                              const std::string& schema_name, const std::string& table_name)
-  {
+}
+
+void TableInfoWindow::load_indexes_data(sancho::db::PostgresConnection &conn,
+                                        const std::string &schema_name,
+                                        const std::string &table_name) {
     indexes_model->clear();
 
     std::shared_ptr<sancho::QueryResult> result =
@@ -165,37 +187,65 @@ void TableInfoWindow::init(sancho::db::PostgresConnection& conn,
                        conn.get_columns_query(schema_name, table_name));
 
     if (!result->success) {
-      return;
+        return;
     }
 
-    const auto& data = result->as_map();
+    const auto &data = result->as_map();
 
-    for (const auto& row_data : data) {
-      Gtk::TreeModel::Row row = *(indexes_model->append());
+    for (const auto &row_data : data) {
+        Gtk::TreeModel::Row row = *(indexes_model->append());
 
-      for (const auto& pair : row_data) {
-        const std::string& key = pair.first;
-        const std::string& value = pair.second;
+        for (const auto &pair : row_data) {
+            const std::string &key = pair.first;
+            const std::string &value = pair.second;
 
-        if (key == "index_name") {
-          row[indexes_columns.col_name] = value;
-        } else if (key == "index_definition") {
-          row[indexes_columns.col_definition] = value;
+            if (key == "index_name") {
+                row[indexes_columns.col_name] = value;
+            } else if (key == "index_definition") {
+                row[indexes_columns.col_definition] = value;
+            }
         }
-      }
     }
-  }
-
-void TableInfoWindow::on_win_show() {
-  notebook_tabs->set_current_page(0);
 }
 
-void TableInfoWindow::on_win_hide() {
+void TableInfoWindow::load_stats_data(sancho::db::PostgresConnection &conn,
+                                      const std::string &schema_name,
+                                      const std::string &table_name) {
+    stats_model->clear();
+
+    std::shared_ptr<sancho::QueryResult> result =
+        conn.run_query(sancho::QueryType::NonTransaction,
+                       conn.get_table_stats_query(schema_name, table_name),
+                       conn.get_columns_query(schema_name, table_name));
+
+    if (!result->success) {
+        return;
+    }
+
+    const auto &data = result->as_map();
+
+    for (const auto &row_data : data) {
+        for (const auto &pair : row_data) {
+            Gtk::TreeModel::Row row = *(stats_model->append());
+
+            const std::string &key = pair.first;
+            const std::string &value = pair.second;
+
+            row[stats_columns.col_name] = key;
+            row[stats_columns.col_value] = value;
+
+            if (descriptions.find(key) != descriptions.end()) {
+                row[stats_columns.col_description] = descriptions.at(key);
+            }
+        }
+    }
 }
 
-void TableInfoWindow::on_btn_close_clicked() {
-  hide();
-}
+void TableInfoWindow::on_win_show() { notebook_tabs->set_current_page(0); }
+
+void TableInfoWindow::on_win_hide() {}
+
+void TableInfoWindow::on_btn_close_clicked() { hide(); }
 
 } // namespace sancho
 }
--- src/ui/gtk/win_table_info.hpp
+++ src/ui/gtk/win_table_info.hpp
@@ -8,24 +8,32 @@
 namespace sancho {
 namespace ui {
 namespace gtk {
+
 class TableInfoWindow : public Gtk::Window {
   public:
     TableInfoWindow(BaseObjectType *cobject,
                     const Glib::RefPtr<Gtk::Builder> &builder);
     virtual ~TableInfoWindow(){};
 
-  void init(sancho::db::PostgresConnection& conn, const std::string& schema_name, const std::string& table_name);
+    void init(sancho::db::PostgresConnection &conn,
+              const std::string &schema_name, const std::string &table_name);
 
   private:
     void on_win_show();
     void on_win_hide();
     void on_btn_close_clicked();
-    void load_columns_data(sancho::db::PostgresConnection& conn,
-                           const std::string& schema_name, const std::string& table_name);
-    void load_constraints_data(sancho::db::PostgresConnection& conn,
-                               const std::string& schema_name, const std::string& table_name);
-    void load_indexes_data(sancho::db::PostgresConnection& conn,
-                               const std::string& schema_name, const std::string& table_name);
+    void load_columns_data(sancho::db::PostgresConnection &conn,
+                           const std::string &schema_name,
+                           const std::string &table_name);
+    void load_constraints_data(sancho::db::PostgresConnection &conn,
+                               const std::string &schema_name,
+                               const std::string &table_name);
+    void load_indexes_data(sancho::db::PostgresConnection &conn,
+                           const std::string &schema_name,
+                           const std::string &table_name);
+    void load_stats_data(sancho::db::PostgresConnection &conn,
+                         const std::string &schema_name,
+                         const std::string &table_name);
 
     Glib::RefPtr<Gtk::Builder> builder;
 
@@ -33,17 +41,18 @@ class TableInfoWindow : public Gtk::Window {
     Gtk::Box *box_columns;
     Gtk::Box *box_constraints;
     Gtk::Box *box_indexes;
+    Gtk::Box *box_stats;
     Gtk::Button *btn_close;
 
-  // Schema tab
+    // Schema tab
     class SchemaColumns : public Gtk::TreeModel::ColumnRecord {
       public:
         SchemaColumns() {
-          add(col_pos);
-          add(col_name);
-          add(col_type);
-          add(col_is_nullable);
-          add(col_default);
+            add(col_pos);
+            add(col_name);
+            add(col_type);
+            add(col_is_nullable);
+            add(col_default);
         }
 
         Gtk::TreeModelColumn<Glib::ustring> col_pos;
@@ -58,12 +67,12 @@ class TableInfoWindow : public Gtk::Window {
     Gtk::TreeView tree_schema;
     Glib::RefPtr<Gtk::TreeStore> schema_model;
 
-  // Constraints tab
+    // Constraints tab
     class ConstraintsColumns : public Gtk::TreeModel::ColumnRecord {
       public:
         ConstraintsColumns() {
-          add(col_name);
-          add(col_check);
+            add(col_name);
+            add(col_check);
         }
 
         Gtk::TreeModelColumn<Glib::ustring> col_name;
@@ -75,12 +84,12 @@ class TableInfoWindow : public Gtk::Window {
     Gtk::TreeView tree_constraints;
     Glib::RefPtr<Gtk::TreeStore> constraints_model;
 
-  // Indexes tab
+    // Indexes tab
     class IndexesColumns : public Gtk::TreeModel::ColumnRecord {
       public:
         IndexesColumns() {
-          add(col_name);
-          add(col_definition);
+            add(col_name);
+            add(col_definition);
         }
 
         Gtk::TreeModelColumn<Glib::ustring> col_name;
@@ -92,6 +101,58 @@ class TableInfoWindow : public Gtk::Window {
     Gtk::TreeView tree_indexes;
     Glib::RefPtr<Gtk::TreeStore> indexes_model;
 
+    // Stats tab
+    class StatsColumns : public Gtk::TreeModel::ColumnRecord {
+      public:
+        StatsColumns() {
+            add(col_name);
+            add(col_description);
+            add(col_value);
+        }
+
+        Gtk::TreeModelColumn<Glib::ustring> col_name;
+        Gtk::TreeModelColumn<Glib::ustring> col_description;
+        Gtk::TreeModelColumn<Glib::ustring> col_value;
+    };
+
+    StatsColumns stats_columns;
+    Gtk::ScrolledWindow scrolled_stats;
+    Gtk::TreeView tree_stats;
+    Glib::RefPtr<Gtk::TreeStore> stats_model;
+
+    const std::unordered_map<std::string, std::string> descriptions = {
+        {"relid", "OID of a table"},
+        {"schemaname", "Name of the schema that this table is in"},
+        {"relname", "Name of this table"},
+        {"seq_scan", "Number of sequential scans initiated on this table"},
+        {"seq_tup_read", "Number of live rows fetched by sequential scans"},
+        {"idx_scan", "Number of index scans initiated on this table"},
+        {"idx_tup_fetch", "Number of live rows fetched by index scans"},
+        {"n_tup_ins", "Number of rows inserted"},
+        {"n_tup_upd", "Number of rows updated (includes HOT updated rows)"},
+        {"n_tup_del", "Number of rows deleted"},
+        {"n_tup_hot_upd", "Number of rows HOT updated (i.e., with no separate "
+                          "index update required)"},
+        {"n_live_tup", "Estimated number of live rows"},
+        {"n_dead_tup", "Estimated number of dead rows"},
+        {"n_mod_since_analyze", "Estimated number of rows modified since this "
+                                "table was last analyzed"},
+        {"last_vacuum", "Last time at which this table was manually vacuumed "
+                        "(not counting VACUUM FULL)"},
+        {"last_autovacuum",
+         "Last time at which this table was vacuumed by the autovacuum daemon"},
+        {"last_analyze", "Last time at which this table was manually analyzed"},
+        {"last_autoanalyze",
+         "Last time at which this table was analyzed by the autovacuum daemon"},
+        {"vacuum_count", "Number of times this table has been manually "
+                         "vacuumed (not counting VACUUM FULL)"},
+        {"autovacuum_count", "Number of times this table has been vacuumed by "
+                             "the autovacuum daemon"},
+        {"analyze_count",
+         "Number of times this table has been manually analyzed"},
+        {"autoanalyze_count", "Number of times this table has been analyzed by "
+                              "the autovacuum daemon"},
+    };
 };
 } // namespace sancho
 }
